1f44a155844f5c37c0cd4e0855661946b30ef8cd
david_logan <PE4-fby-o2g-vMN>
2020-08-04 08:29:15 -0600

    Add install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if ! type git >& /dev/null
+then
+	apt-get install -y git
+fi
+
+if ! type node >& /dev/null
+then
+	curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+	apt-get install -y nodejs
+fi
+
+cd /opt
+
+if ! [[ -d stockfish2 ]]
+then
+	git clone https://github.com/djlogan2/stockfish2.git
+fi
+
+cd /opt/stockfish2
+
+if ! [[ -d node_modules ]]
+then
+	npm install
+fi
+chmod +x docker/*
+node main.js >& /var/log/stockfish2.log &
+disown
